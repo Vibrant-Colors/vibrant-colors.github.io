fe37375d34c3621c12b568dfa2a55d7dafcc4e6b
MDutro <mdutrolaw@gmail.com>
2020-04-22 01:10:58 -0500

    Add image chooser.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Colors from './Colors'
 
 const App = () => {
+  
+  const [image, setImage] = useState("/placeholder.png")
+
+  const onChange = event => {
+    event.preventDefault()
+    const fileURL = URL.createObjectURL(event.target.files[0])
+    setImage(fileURL)
+  }
+  
   return (
     <div className="app container">
       <img className="image" src="/mountainlake.jpg" alt=""/>
@@ -13,6 +22,14 @@ const App = () => {
       <Colors img="/irelandPark.jpg"/>
       <img className="image" src="/nightumbrella.jpg" alt=""/>
       <Colors img="/nightumbrella.jpg" />
+      <img  className="image" src={image} alt=""/>
+      <Colors img={image} />
+      <div>
+        <label for="file-upload" class="button">
+          Choose Image...
+        </label>
+        <input id="file-upload" type="file" onChange={onChange}/>
+      </div>
     </div>
   );
 }
