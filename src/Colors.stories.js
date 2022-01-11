c95aa8c85a1ea4158fdab5b308f3c71611a9ad66
sam <ruwesam@gmail.com>
2022-01-11 07:22:55 -0500

    Configure Storybook and Add Initial Stories

---
+++ src/Colors.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Colors } from './Colors';
+
+export default {
+    title: 'Colors Component',
+    component: Colors
+};
+
+export const Primary = () => <Colors file='mountainlake.jpg'/>
---
+++ src/VibrantColors.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import VibrantColors from './VibrantColors';
+
+export default {
+    title: 'VibrantColors Component',
+    component: VibrantColors
+};
+
+export const Primary = () => <VibrantColors img='mountainlake.jpg'/>
