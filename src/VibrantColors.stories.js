6d7ab11646291f8b0cca0da0b8c4015530a4e5ef
sam <ruwesam@gmail.com>
2022-01-11 08:37:00 -0500

    Add object with image links, update stories

--- src/VibrantColors.stories.js
+++ src/VibrantColors.stories.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 import VibrantColors from './VibrantColors';
+import { Images } from './Images';
 
 export default {
     title: 'VibrantColors Component',
-    component: VibrantColors
+    component: VibrantColors,
+    argTypes: {
+        img: {
+            control: {
+                type: 'radio',
+                labels: Object.fromEntries(Object.entries(Images).map(([key, val]) => [val, key]))
+            },
+            options: Object.entries(Images).map(([, image]) => image)
+        }
+    },
+    args: {
+        img: Images.MountainLake
+    }
 };
 
-export const Primary = () => <VibrantColors img='mountainlake.jpg'/>
+export const Primary = args => <VibrantColors {...args}/>
