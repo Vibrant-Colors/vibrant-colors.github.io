8d9b7f6ef2388935121e6d980e79990f976d7e9a
sam <ruwesam@gmail.com>
2022-02-08 15:30:14 -0500

    Refactor Vibrant Colors to allow for typing

--- src/components/Colors/VibrantColors/VibrantColors.tsx
+++ src/components/Colors/VibrantColors/VibrantColors.tsx
@@ -1,48 +1,47 @@
 import React, { useCallback, useEffect, useState } from "react";
 import styles from "./VibrantColors.module.css";
 import Vibrant from "node-vibrant";
+import { Palette } from "node-vibrant/lib/color";
 
 export interface VibrantColorsProps {
   img: string;
 }
 
-export const VibrantColors = ({ img }: VibrantColorsProps) => {
-  const [colors, setColors] = useState({
-    DarkMuted: [],
-    DarkVibrant: [],
-    LightMuted: [],
-    LightVibrant: [],
-    Muted: [],
-    Vibrant: [],
-  } as any);
+interface VibrantColorsState {
+  DarkMuted: string;
+  DarkVibrant: string;
+  LightMuted: string;
+  LightVibrant: string;
+  Muted: string;
+  Vibrant: string;
+}
 
-  const runVibrant = useCallback(
-    (arr) => {
-      const _rgbToString = (_rgb: Array<any>) =>
-        `rgb(${_rgb[0]}, ${_rgb[1]}, ${_rgb[2]})`;
-      Vibrant.from(img)
-        .getPalette()
-        .then((palette) =>
-          setColors({
-            // @ts-ignore
-            DarkMuted: _rgbToString(palette.DarkMuted._rgb),
-            // @ts-ignore
-            DarkVibrant: _rgbToString(palette.DarkVibrant._rgb),
-            // @ts-ignore
-            LightMuted: _rgbToString(palette.LightMuted._rgb),
-            // @ts-ignore
-            LightVibrant: _rgbToString(palette.LightVibrant._rgb),
-            // @ts-ignore
-            Muted: _rgbToString(palette.Muted._rgb),
-            // @ts-ignore
-            Vibrant: _rgbToString(palette.Vibrant._rgb),
-          })
-        );
-    },
-    [img]
+const DEFAULT_COLOR = "#000000";
+
+class VibrantColorsState implements VibrantColorsState {
+  constructor(colors?: Palette) {
+    this.DarkMuted = colors?.DarkMuted?.getHex() ?? DEFAULT_COLOR;
+    this.DarkVibrant = colors?.DarkVibrant?.getHex() ?? DEFAULT_COLOR;
+    this.LightMuted = colors?.LightMuted?.getHex() ?? DEFAULT_COLOR;
+    this.LightVibrant = colors?.LightVibrant?.getHex() ?? DEFAULT_COLOR;
+    this.Muted = colors?.Muted?.getHex() ?? DEFAULT_COLOR;
+    this.Vibrant = colors?.Vibrant?.getHex() ?? DEFAULT_COLOR;
+  }
+}
+
+export const VibrantColors = ({ img }: VibrantColorsProps) => {
+  const [colors, setColors] = useState<VibrantColorsState>(
+    new VibrantColorsState()
   );
 
-  // @ts-ignore
+  const runVibrant = useCallback(() => {
+    Vibrant.from(img)
+      .getPalette()
+      .then((palette) => {
+        setColors(new VibrantColorsState(palette));
+      });
+  }, [img]);
+
   useEffect(() => runVibrant(), [runVibrant]);
 
   return (
