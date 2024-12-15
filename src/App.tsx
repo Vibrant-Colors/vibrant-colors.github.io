47c35506a16b9276ef0826a16445974025591113
Corbin Crutchley <git@crutchcorn.dev>
2024-12-15 10:13:13 -0800

    chore: add more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,42 @@
-import {ChangeEvent, useState} from 'react';
-import styles from './App.module.css';
-import {Colors} from './Colors';
-import {Images} from './Images';
-import {Title} from "./title.tsx";
+import { ChangeEvent, useState } from "react";
+import styles from "./App.module.css";
+import { Colors } from "./Colors";
+import { Images } from "./Images";
+import { Title } from "./title.tsx";
+import { ExamplesTitle } from "./examples-title.tsx";
 
 const App = () => {
-
-  const [image, setImage] = useState(Images.MountainLake)
+  const [image, setImage] = useState(Images.MountainLake);
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault()
-    const fileURL = URL.createObjectURL(event.target.files![0])
-    setImage(fileURL)
-  }
+    event.preventDefault();
+    const fileURL = URL.createObjectURL(event.target.files![0]);
+    setImage(fileURL);
+  };
 
   return (
     <div>
+      <Title title={"Vibrant"} />
 
-    <Title title={"Vibrant"}/>
+      <div className={styles.app}>
+        <Colors file={image} className={styles.fullSize} />
 
-    <div className={styles.app}>
-      <Colors file={image} className={styles.fullSize}/>
+        <div className={`${styles.fullSize} ${styles.flex}`}>
+          <label htmlFor="file-upload" className={styles.button}>
+            Upload your own
+          </label>
+          <input id="file-upload" type="file" onChange={onChange} />
+        </div>
 
-      <div className={`${styles.fullSize} ${styles.flex}`}>
-        <label htmlFor="file-upload" className={styles.button}>
-         Upload your own
-        </label>
-        <input id="file-upload" type="file" onChange={onChange}/>
-      </div>
+        <div className={`${styles.fullSize} ${styles.flex}`}>
+          <ExamplesTitle title={"Examples"} />
+        </div>
 
-      <Colors file={Images.PeacockFeathers}/>
-      <Colors file={Images.IrelandPark}/>
-
+        <Colors file={Images.PeacockFeathers} />
+        <Colors file={Images.IrelandPark} />
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default App;
